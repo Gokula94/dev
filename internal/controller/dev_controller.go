9cb8009fb259658464433568f19790833ae41669
Gokula94 <mailgokul94@gmail.com>
2024-03-28 10:15:45 +0000

    patch15

--- internal/controller/dev_controller.go
+++ internal/controller/dev_controller.go
@@ -18,14 +18,15 @@ package controller
 
 import (
 	"context"
-	"flag"
 	"fmt"
-	"path/filepath"
 
 	apiv1alpha1 "dev/api/v1alpha1"
 
 	"k8s.io/apimachinery/pkg/runtime"
-	"k8s.io/client-go/util/homedir"
+	"k8s.io/client-go/informers"
+	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/rest"
+	"k8s.io/client-go/tools/cache"
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/client"
 	"sigs.k8s.io/controller-runtime/pkg/log"
@@ -61,58 +62,43 @@ func (r *DevReconciler) Reconcile(ctx context.Context, req ctrl.Request) (ctrl.R
 	fmt.Println("Reconcile is called for testing")
 	logger.Info(fmt.Sprintf("Pod created is %v", req.NamespacedName))
 
-	dev := &apiv1alpha1.Dev{}
+	//dev := &apiv1alpha1.Dev{}
 
-	err := r.Get(ctx, req.NamespacedName, dev)
+	//err := r.Get(ctx, req.NamespacedName, dev)
 
-	var kubeconfig *string
-	if home := homedir.HomeDir(); home != "" {
-		kubeconfig = flag.String("kubeconfig", filepath.Join(home, ".kube", "config"), "(optional) absolute path to the kubeconfig file")
-	} else {
-		kubeconfig = flag.String("kubeconfig", "", "absolute path to the kubeconfig file")
+	config, err := rest.InClusterConfig()
+	if err != nil {
+		panic(err.Error())
 	}
-	flag.Parse()
+	// creates the clientset
+	clientset, err := kubernetes.NewForConfig(config)
+	if err != nil {
+		panic(err.Error())
+	}
 
-	// use the current context in kubeconfig
-	//config, err := clientcmd.BuildConfigFromFlags("", *kubeconfig)
+	// create the clientset
+	stopper := make(chan struct{})
+	defer close(stopper)
 
-	fmt.Println(kubeconfig)
+	factory := informers.NewSharedInformerFactory(clientset, 0)
 
-	/*
-		// use the current context in kubeconfig
-		config, err := clientcmd.BuildConfigFromFlags("", *kubeconfig)
+	informer := factory.Core().V1().Pods().Informer()
 
-		if err != nil {
-			_ = fmt.Errorf("failed to config %s", err.Error())
-			config, err = rest.InClusterConfig()
-			if err != nil {
-				panic(err.Error())
-			}
-		}
-		// create the clientset
-		clientset, err := kubernetes.NewForConfig(config)
-		stopper := make(chan struct{})
-		defer close(stopper)
+	informer.AddEventHandler(cache.ResourceEventHandlerFuncs{
+		AddFunc: func(obj interface{}) {
+			fmt.Println("add event")
+		},
+		UpdateFunc: func(obj1, obj2 interface{}) {
+			fmt.Println("update event")
+		},
+		DeleteFunc: func(obj interface{}) {
+			fmt.Println("delete event")
+		},
+	})
 
-		factory := informers.NewSharedInformerFactory(clientset, 0)
+	go informer.Run(stopper)
+	<-stopper
 
-		informer := factory.Core().V1().Pods().Informer()
-
-		informer.AddEventHandler(cache.ResourceEventHandlerFuncs{
-			AddFunc: func(obj interface{}) {
-				fmt.Println("add event")
-			},
-			UpdateFunc: func(obj1, obj2 interface{}) {
-				fmt.Println("update event")
-			},
-			DeleteFunc: func(obj interface{}) {
-				fmt.Println("delete event")
-			},
-		})
-
-		go informer.Run(stopper)
-		<-stopper
-	*/
 	return ctrl.Result{}, err
 }
 
